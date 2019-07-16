67b8e901a9e360cab8e25a19ea7159c617e2bf43
El amirchi rachid <rachid.elamirchi@gmail.com>
2019-07-16 16:33:02 +0200

    correction php 7

--- includes/login-exec.php
+++ includes/login-exec.php
@@ -12,25 +12,25 @@
 	$errflag = false;
 	
 	//Connect to mysql server
-	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-	if(!$link) {
-		die('Failed to connect to server: ' . mysql_error());
-	}
-	
-	//Select database
-	$db = mysql_select_db(DB_DATABASE);
-	if(!$db) {
-		die("Unable to select database");
-	}
-	
-	//Function to sanitize values received from the form. Prevents SQL injection
-	function clean($str) {
-		$str = @trim($str);
-		if(get_magic_quotes_gpc()) {
-			$str = stripslashes($str);
-		}
-		return mysql_real_escape_string($str);
-	}
+    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+    if(!$link) {
+        die('Failed to connect to server: ' . mysql_error());
+    }
+
+    //Select database
+    $db = mysqli_select_db($link, DB_DATABASE);
+    if(!$db) {
+        die("Unable to select database");
+    }
+
+    //Function to sanitize values received from the form. Prevents SQL injection
+    function clean($str, $link) {
+        $str = @trim($str);
+        if(get_magic_quotes_gpc()) {
+            $str = stripslashes($str);
+        }
+        return mysqli_real_escape_string($link, $str);
+    }
 	
 	//Sanitize the POST values
 	$username = clean($_POST['username']);
